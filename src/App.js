685d291f05ca2e4280933680fda86c9a72ce2148
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-06-28 17:40:56 +0100

    started pacman movement function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import wall from "../src/images/wall.png";
 import coin from "../src/images/coin.png";
-import pacmann from "../src/images/pacman.png";
+import pacman from "../src/images/pacman.png";
 import bg from "../src/images/bg.png";
 import ghost from "../src/images/ghost2.png";
 import "./App.css";
@@ -19,6 +19,28 @@ const PacManGame = () => {
     [1, 2, 2, 2, 2, 2, 1, 4, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   ]);
+  const [gameOver, setGameOver] = useState(false);
+  const handleKeyDown = (event) => {
+    if (gameOver) {
+      return;
+    }
+    if (
+      event.keyCode === 37 &&
+      pacman.x > 0 &&
+      map[pacman.y][pacman.x - 1] !== 1
+    ) {
+      setMap((prevMap) => {
+        const newMap = [...prevMap];
+        newMap[pacman.y][pacman.x] = 3;
+        setPacman((prevPacman) => ({
+          ...prevPacman,
+          x: prevPacman.x - 1,
+        }));
+        newMap[pacman.y][pacman.x - 1] = 5;
+        return newMap;
+      });
+    }
+  };
 };
 
 export default PacManGame;
